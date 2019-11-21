3e13325531a850c65254bdc42b1ed5a182497d77
Anthony Zaldana <anthonyzaldana@gmail.com>
2019-11-21 07:13:50 -0500

    update

---
+++ app/src/main/java/com/example/arekie/ui/menu/MenuFragment.java
@@ -0,0 +1,28 @@
+package com.example.arekie.ui.menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.arekie.R;
+
+public class MenuFragment extends Fragment {
+
+    private MenuViewModel menuViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        menuViewModel =
+                ViewModelProviders.of(this).get(MenuViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_menu, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/arekie/ui/menu/MenuViewModel.java
@@ -0,0 +1,21 @@
+package com.example.arekie.ui.menu;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MenuViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MenuViewModel() {
+        mText = new MutableLiveData<>();
+
+
+
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
